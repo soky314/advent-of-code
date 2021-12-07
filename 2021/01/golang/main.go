48a2fa5def3eb52bf7352c4772a0f6fcbcc03aa3
soky314 <soky314@gmail.com>
2021-12-07 23:53:41 +0100

    feat(2021-01-Golang): add implementation of part one CountNumberOfIncrease

--- 2021/01/golang/main.go
+++ 2021/01/golang/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 )
 
+const MaxInt = int((^uint(0)) >> 1)
+
 func main() {
-	file, err := os.Open("/data/input.txt")
+	file, err := os.Open("data/input.txt")
 	if err != nil {
 		panic(fmt.Errorf("failed to open file, err: %w", err))
 	}
@@ -16,5 +21,29 @@ func main() {
 }
 
 func CountNumberOfIncrease(r io.Reader) (count int) {
-	return 0
+	scan := bufio.NewScanner(r)
+
+	var (
+		index int
+		last  = MaxInt
+	)
+	for scan.Scan() {
+		index++
+		if err := scan.Err(); err != nil {
+			if !errors.Is(err, io.EOF) {
+				panic(fmt.Errorf("in reading line(%d), err: %w", index, err))
+			}
+			break
+		}
+		actual, err := strconv.Atoi(scan.Text())
+		if err != nil {
+			panic(fmt.Errorf("in parsing line(%d), err: %w", index, err))
+		}
+		if actual > last {
+			count++
+		}
+		last = actual
+	}
+
+	return count
 }
