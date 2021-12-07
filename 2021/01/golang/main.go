7a460441c720167f841d7777fe2b8fb14bf40f39
soky314 <soky314@gmail.com>
2021-12-08 00:27:38 +0100

    chore(2021-01-Golang): add implementation of part TWO: CountNumberOfWindowIncrease

--- 2021/01/golang/main.go
+++ 2021/01/golang/main.go
@@ -18,6 +18,11 @@ func main() {
 	}
 
 	fmt.Println("Number of increase: ", CountNumberOfIncrease(file))
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		panic(fmt.Errorf("failed to rewind opened file, err: %w", err))
+	}
+	fmt.Println("Number of window increase: ", CountNumberOfWindowIncrease(file))
 }
 
 func CountNumberOfIncrease(r io.Reader) (count int) {
@@ -49,5 +54,38 @@ func CountNumberOfIncrease(r io.Reader) (count int) {
 }
 
 func CountNumberOfWindowIncrease(r io.Reader) (count int) {
-	return 0
+	scan := bufio.NewScanner(r)
+
+	var (
+		index  int
+		last   = MaxInt
+		window = [3]int{0, 0, 0}
+	)
+	sumWindow := func() int {
+		return window[0] + window[1] + window[2]
+	}
+	for scan.Scan() {
+		index++
+		if err := scan.Err(); err != nil {
+			if !errors.Is(err, io.EOF) {
+				panic(fmt.Errorf("in reading line(%d), err: %w", index, err))
+			}
+			break
+		}
+		tmp, err := strconv.Atoi(scan.Text())
+		if err != nil {
+			panic(fmt.Errorf("in parsing line(%d), err: %w", index, err))
+		}
+		window[(index-1)%3] = tmp
+		if index < 3 {
+			continue
+		}
+		actual := sumWindow()
+		if actual > last {
+			count++
+		}
+		last = actual
+	}
+
+	return count
 }
