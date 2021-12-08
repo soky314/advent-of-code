d34121d25cb454758060d405f7f63755cdc49814
soky314 <soky314@gmail.com>
2021-12-08 23:33:28 +0100

    feat(2021-03-Golang): add implementation of part ONE of day 03

--- 2021/03/golang/main.go
+++ 2021/03/golang/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 )
 
 const day = "03" // todo: add day of advent calendar
@@ -23,7 +26,59 @@ func main() {
 }
 
 func SolvePartOne(r io.Reader) int {
-	return 0
+	scan := bufio.NewScanner(r)
+
+	var (
+		index  int
+		counts = map[int][2]int{}
+	)
+	for scan.Scan() {
+		index++
+		if err := scan.Err(); err != nil {
+			if !errors.Is(err, io.EOF) {
+				panic(fmt.Errorf("in reading line(%d), err: %w", index, err))
+			}
+			break
+		}
+		for i, value := range scan.Text() {
+			positionCount := counts[i]
+			switch value {
+			case '0':
+				positionCount[0]++
+			case '1':
+				positionCount[1]++
+			default:
+				panic(fmt.Errorf("in parsing line(%d), got: %s ", index, string(value)))
+			}
+			counts[i] = positionCount
+		}
+	}
+	var (
+		gamaBinary    = make([]rune, len(counts))
+		epsilonBinary = make([]rune, len(counts))
+	)
+
+	for i, value := range counts {
+		if value[1] > value[0] {
+			gamaBinary[i] = '1'
+			epsilonBinary[i] = '0'
+		} else {
+			gamaBinary[i] = '0'
+			epsilonBinary[i] = '1'
+		}
+	}
+
+	gama, err := strconv.ParseInt(string(gamaBinary), 2, 64)
+	if err != nil {
+		panic(fmt.Errorf("in parsing gama(%s), err: %w ", string(gamaBinary), err))
+
+	}
+	epsilon, err := strconv.ParseInt(string(epsilonBinary), 2, 64)
+	if err != nil {
+		panic(fmt.Errorf("in parsing epsilon(%s), err: %w ", string(epsilonBinary), err))
+
+	}
+	return int(gama * epsilon)
 }
 
 func SolvePartTwo(r io.Reader) int {
